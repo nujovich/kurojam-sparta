4900fc8b34396adf8ec61f843a7924392359ae80
Cesar Muñoz <cesar.alberto.munoz@gmail.com>
2023-08-18 09:13:17 +0200

    feat: adding clerk for login users (#17)
    * feat: connect client with server
    
    * feat: adding clerk for login users

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -5,8 +5,11 @@ import { ButtonDemo } from "./components/ButtonDemo";
 import { ThemeProvider } from "./components/theme-provider";
 import { Button } from "./components/ui/button";
 import { createOne, getAll } from "./lib/entity";
+import { SignedIn, SignedOut, UserButton, useClerk } from "@clerk/clerk-react";
 
 function App() {
+  const clerk = useClerk();
+  
   const handleCreateMeme = async () => {
     const response = await createOne("memes", {
       name: "Meme",
@@ -51,12 +54,29 @@ function App() {
         </p>
         {/* Esto de abajo es la llamada del cloudinary que muestra la info tmb */}
         <ImageData />
-        <Button className="mt-2" onClick={handleCreateMeme}>
-          Create Meme Record
-        </Button>
-        <Button className="mt-2" onClick={handleGetAllMemes}>
-          Get All Meme Records
-        </Button>
+        <div className="flex justify-center">
+          <h2 className="text-2xl">Meme Records</h2>
+          <Button className="mt-2" onClick={handleCreateMeme}>
+            Create Meme Record
+          </Button>
+          <Button className="mt-2" onClick={handleGetAllMemes}>
+            Get All Meme Records
+          </Button>
+        </div>
+        <div className="flex justify-center">
+          <h2 className="text-2xl">Login with clerk</h2>
+          <SignedOut>
+            <Button className="mt-2" onClick={() => clerk.openSignUp()}>
+              Sign Up
+            </Button>
+            <Button className="mt-2" onClick={() => clerk.openSignIn()}>
+              Sign In
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+        </div>
       </div>
     </ThemeProvider>
   );
--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/globals.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles/globals.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ClerkProvider publishableKey={publishableKey}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
