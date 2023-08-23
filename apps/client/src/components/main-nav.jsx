de9898a6d267ba1a58608e62bc2e54523698bed1
Ivan <ivanmeyer1991@gmail.com>
2023-08-23 14:42:13 -0300

    mobile-view styles

--- apps/client/src/components/main-nav.jsx
+++ apps/client/src/components/main-nav.jsx
@@ -13,28 +13,40 @@ export function MainNav({ className, ...props }) {
           className="h-10"
         />
       </Link>
-      <h1 className="text-2xl font-bold">DUCKUROPHY</h1>
+      <h1 className="text-2xl font-bold hidden md:block">DUCKUROPHY</h1>
       <nav
         className={cn('flex items-center space-x-4 lg:space-x-6', className)}
         {...props}
       >
         <Link
           to="/"
-          className="text-sm font-medium transition-colors hover:font-bold"
+          className="text-sm font-medium transition-colors hover:font-bold "
         >
           Home
         </Link>
         <Link
           to="/rate"
-          className="text-sm font-medium transition-colors hover:font-bold"
+          className="text-sm font-medium transition-colors hover:font-bold hidden md:block"
         >
           Rate Memes
         </Link>
         <Link
-          to="/About"
-          className="text-sm font-medium transition-colors hover:font-bold"
+          to="/rate"
+          className="text-sm font-medium transition-colors hover:font-bold md:hidden"
+        >
+          Rate
+        </Link>
+        <Link
+          to="/about"
+          className="text-sm font-medium transition-colors hover:font-bold hidden md:block"
         >
           About Us
+        </Link>
+        <Link
+          to="/about"
+          className="text-sm font-medium transition-colors hover:font-bold md:hidden"
+        >
+          About
         </Link>
       </nav>
     </div>
