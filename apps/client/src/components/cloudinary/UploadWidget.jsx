a6388be3a32d193e7f9605a6c2b8c378de9938fc
iván Meyer <37188867+IvanMJs@users.noreply.github.com>
2023-08-23 17:11:17 -0300

    mobile-view styles (#42)

--- apps/client/src/components/cloudinary/UploadWidget.jsx
+++ apps/client/src/components/cloudinary/UploadWidget.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import { Button } from "@/components/ui/button";
+import React, { Component } from 'react'
+import { Button } from '@/components/ui/button'
 
 class CloudinaryUploadWidget extends Component {
   componentDidMount() {
-    const cloudName = `dtyey9uos`;
-    const uploadPreset = `kofgxo4l`;
+    const cloudName = `dtyey9uos`
+    const uploadPreset = `kofgxo4l`
 
     var myWidget = window.cloudinary.createUploadWidget(
       {
@@ -12,13 +12,13 @@ class CloudinaryUploadWidget extends Component {
         uploadPreset: uploadPreset,
       },
       (error, result) => {
-        if (!error && result && result.event === "success") {
-          this.props.handleImageUpload(result.info.url);
+        if (!error && result && result.event === 'success') {
+          this.props.handleImageUpload(result.info.url)
         }
       }
-    );
-    document.getElementById("upload_widget").addEventListener(
-      "click",
+    )
+    document.getElementById('upload_widget').addEventListener(
+      'click',
       function () {
         myWidget.open()
       },
@@ -28,11 +28,18 @@ class CloudinaryUploadWidget extends Component {
 
   render() {
     return (
-      <div className="flex justify-center items-center">
-        <Button type="button" id="upload_widget">
-          Upload Your Meme
-        </Button>
-      </div>
+      <>
+        <div className="flex justify-center items-center md:hidden">
+          <Button type="button" id="upload_widget">
+            Upload Meme
+          </Button>
+        </div>
+        <div className="flex justify-center items-center hidden md:block">
+          <Button type="button" id="upload_widget">
+            Upload Your Meme
+          </Button>
+        </div>
+      </>
     )
   }
 }
