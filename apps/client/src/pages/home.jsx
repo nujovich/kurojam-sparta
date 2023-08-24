f5cd93d44f0ad073a867a573ff1387f222d866d8
Cesar Munoz <cesar.alberto.munoz@gmail.com>
2023-08-24 07:49:01 +0200

    Fixing Toast

--- apps/client/src/pages/home.jsx
+++ apps/client/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { TrendingUp } from 'lucide-react'
+import { Loader2, TrendingUp } from 'lucide-react'
 import { SignedIn, useUser } from '@clerk/clerk-react'
 import SearchBar from '../components/serachbar/SearchBar'
 import MemeCard from '../components/MemeCard/MemeCard'
@@ -9,11 +9,12 @@ import { createOne, getOne, getAll, request } from '../lib/entity'
 
 function Home() {
   const { user } = useUser()
-  const toast = useToast()
+  const { toast } = useToast()
 
   const [image, setImage] = useState()
   const [trending, setTrending] = useState([])
   const [isSearching, setIsSearching] = useState(false)
+  const [isSaving, setIsSaving] = useState(false)
 
   useEffect(() => {
     const fetchTrendring = async () => {
@@ -28,17 +29,22 @@ function Home() {
   }, [])
 
   const handleOnSaveDuck = async () => {
+    setIsSaving(true)
     const res = await createOne('memes', {
       url: image,
       prompt: 'Duck',
       userId: user?.id,
     })
-    if (!res.error) {
-      toast({
-        title: 'Duck created',
-        description: 'Duck created successfully',
-      })
+    if (res.error) {
+      console.log(res.error)
+      return
     }
+
+    setIsSaving(false)
+    toast({
+      title: 'Duck created',
+      description: 'Duck created successfully',
+    })
   }
 
   const handleSearch = async (prompt) => {
@@ -60,13 +66,23 @@ function Home() {
         <div className="flex flex-col">
           <SearchBar onClick={handleSearch} isLoading={isSearching} />
           {image && (
-            <div className="flex flex-wrap gap-4">
-              <img
-                src={image}
-                alt="Image"
-                className="w-auto h-auto max-w-full max-h-96"
-              />
-            </div>
+            <>
+              <div className="flex flex-wrap gap-4">
+                <img
+                  src={image}
+                  alt="Image"
+                  className="w-auto h-auto max-w-full max-h-96"
+                />
+              </div>
+              <div className="flex flex-wrap gap-4">
+                <Button className="mt-2" onClick={handleOnSaveDuck}>
+                  {isSaving && (
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  {!isSaving && 'Save Duck'}
+                </Button>
+              </div>
+            </>
           )}
         </div>
       </SignedIn>
@@ -84,9 +100,6 @@ function Home() {
               alt={meme.name}
               className="w-auto h-auto max-w-full max-h-96"
             />
-            <Button className="mt-2" onClick={() => handleOnSaveDuck()}>
-              Save Duck
-            </Button>
           </div>
         ))}
       </div>
